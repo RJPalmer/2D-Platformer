63dce8d323dcd0c6a45dd941439648d50700609c
Robert Palme <robert.palmer@adviserms.com>
2017-11-15 19:08:49 -0500

    Movement Added
    Basic Left & Right Movement added to Dummy Character

--- Assets/Scripts/PlayerUIControl.cs
+++ Assets/Scripts/PlayerUIControl.cs
@@ -4,13 +4,57 @@ using UnityEngine;
 
 public class PlayerUIControl : MonoBehaviour {
 
+    public float playerSpeed;
+    private KeyCode lastKeyPressed;
 	// Use this for initialization
 	void Start () {
-		
+        lastKeyPressed = new KeyCode();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        float moveHorizontal;
+
+        //First key press is Right Arrow
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+            transform.Translate(moveHorizontal, 0, 0);
+            playerSpeed += Time.deltaTime;
+            lastKeyPressed = KeyCode.RightArrow;
+            
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+            transform.Translate(moveHorizontal, 0, 0);
+            playerSpeed += Time.deltaTime;
+            lastKeyPressed = KeyCode.RightArrow;
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && lastKeyPressed.CompareTo(KeyCode.RightArrow).Equals(0))
+        {
+            playerSpeed = 1;
+            moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+            transform.Translate(moveHorizontal, 0, 0);
+            playerSpeed += Time.deltaTime;
+            lastKeyPressed = KeyCode.LeftArrow;
+            
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+            transform.Translate(moveHorizontal, 0, 0);
+            playerSpeed += Time.deltaTime;
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }
+
+    }
 }
---
+++ Assets/Scripts/UI Controls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class UIControls : MonoBehaviour {
+
+    public float speed;
+    public float jumpSpeed;
+    public float gravity;
+
+
+	// Use this for initialization
+	public void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        float moveHorizontal;
+        
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            transform.Translate(moveHorizontal, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            transform.Translate(0, moveHorizontal, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+        }
+	}
+}
